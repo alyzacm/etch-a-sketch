8d0dc53d5ba95b01661267ac59ff2e1925772e2e
alyzacm <amalu001@ucr.edu>
2021-02-04 18:21:33 -0800

    Added grid reset button

--- main.js
+++ main.js
@@ -3,15 +3,19 @@ function createGrid(x){
     const gridContainer = document.getElementById("grid");
     gridContainer.innerHTML = '';
     document.documentElement.style.setProperty("--columns-row", x);
-    
             
     for(let i = 0; i < (x*x); i++){
         let cell = document.createElement('div');
         gridContainer.appendChild(cell).className = "grid-item";
         cell.addEventListener("mouseenter", function() {
-            this.style.backgroundColor = "black";
+            cell.classList.add("hover");
         });
     };
 };
 
-createGrid(30);
+function resetGrid(){
+    var size = prompt("How many squares per side for new grid?");
+    size > 100 ? createGrid(100) : createGrid(size);
+}
+
+createGrid(16);
